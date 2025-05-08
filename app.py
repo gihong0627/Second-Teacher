5fc300b00a515d07c39ed619e8a8da8830bf2004
imsang27 <imsang27@naver.com>
2025-05-08 22:17:14 +0900

    feat: STT 생성 라우트 추가
    - stt_generate_route 블루프린트 추가
    - 기존 STT 라우트와 함께 새로운 생성 라우트 등록

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.stt_route import stt_bp
+from routes.stt_generate_route import stt_gen_bp
 
 app = Flask(__name__)
 app.register_blueprint(stt_bp)
+app.register_blueprint(stt_gen_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
